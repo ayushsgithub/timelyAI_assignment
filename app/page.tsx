5b4028a2eb6c74b6ca781fcf706210effb1bca06
Ayush Kumar <ayushchaurasiya78@gmail.com>
2024-09-16 18:50:31 +0530

    project done

--- app/page.tsx
+++ app/page.tsx
@@ -2,100 +2,60 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="flex justify-center items-center bg-gray-50 overflow-hidden min-h-screen">
+      <div className="flex border-black border-2 rounded-lg flex-col w-full max-w-md min-h-screen bg-white relative shadow-xl">
+        <div className="px-4 mt-7">
+          <div className="flex justify-between items-center">
+            <div className="text-lg font-bold cursor-pointer">{'<'}</div>
+            <Image src="/favicon.ico" width={35} height={20} alt="logo" />
+            <div className="text-lg font-bold cursor-pointer">Help?</div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        <div className="px-4 flex-grow">
+          <h1 className="font-bold text-3xl my-10">
+            Your Professional <br /> Website is Ready
+          </h1>
+          <p className="text-sm tracking-wider text-gray-600 my-6">
+            Start attracting more clients with your <br /> optimized online presence
+          </p>
+
+          <div className="grid grid-cols-2 ">
+            <div className="flex items-center space-x-2">
+              <Image src="https://img.icons8.com/?size=100&id=82769&format=png&color=000000" alt="tick" width={17} height={12} />
+              <span className="text-gray-800">SEO & Keywords</span>
+            </div>
+            <div className="flex items-center space-x-2">
+              <Image src="https://img.icons8.com/?size=100&id=82769&format=png&color=000000" alt="tick" width={17} height={12} />
+              <span className="text-gray-800">Perfect on Mobile</span>
+            </div>
+            <div className="flex items-center space-x-2">
+              <Image src="https://img.icons8.com/?size=100&id=82769&format=png&color=000000" alt="tick" width={17} height={12} />
+              <span className="text-gray-800">Mobile Friendly</span>
+            </div>
+            <div className="flex items-center space-x-2">
+              <Image src="https://img.icons8.com/?size=100&id=82769&format=png&color=000000" alt="tick" width={17} height={12} />
+              <span className="text-gray-800">Get Leads & Bookings</span>
+            </div>
+          </div>
+
+          <div className="flex items-center justify-center bg-gradient-to-b from-white to-pink-200 -mx-4 h-full pt-16 mt-14">
+            <Image
+              src="https://images.unsplash.com/photo-1642752187508-5a6dbfb705c6?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="wallpaper"
+              width={240}
+              height={800}
+              className="rounded-lg object-cover shadow-lg"
+            />
+          </div>
+        </div>
+
+        <div className="bg-white w-full px-4 pb-6 pt-3 absolute bottom-0">
+          <button className="bg-black text-white p-4 rounded-lg w-full font-bold tracking-wider">
+            Review & Publish
+          </button>
+        </div>
+      </div>
+    </main>
   );
 }
